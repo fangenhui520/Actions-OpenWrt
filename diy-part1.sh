e922db24f70ddf2e4035b8e9b9efb877f5ec0719
Fang <82165052+fangenhui520@users.noreply.github.com>
2024-11-17 22:07:01 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,5 +16,11 @@
 # Add a feed source
 #rm -rf ./package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/fangenhui520/luci-theme-argon.git package/lean/luci-theme-argon
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+
+rm -rf ./feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
+rm -rf ./feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box}
+rm -rf ./feeds/luci/applications/luci-app-passwall
+rm -rf  ./feeds/packages/net/natmap
+rm -rf ./feeds/luci/applications/luci-natmap
 sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
-rm -rf ./feeds/packages/net/xray-core && rm -rf  ./feeds/packages/net/natmap
