e55a37645e33a3004d1aee7b27e00bec49e33e1b
Fang <82165052+fangenhui520@users.noreply.github.com>
2024-12-21 17:02:48 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -24,6 +24,8 @@
 # 设置密码为空 sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
 # 设置luci显示型号 sed -i 's/GeHua GHL-R-001/GeHuaLian GHL-R-01/g' target/linux/ramips/dts/mt7621_gehua_ghl-r-001.dts
 
+sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=5.10/g' target/linux/ramips/Makefile
+
 #sed -i "s/OpenWrt /Fang build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings # 版本号里显示自己时间
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 #sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
