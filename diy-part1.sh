2e4804cbf81a066de114eae9eb138820e47c2a5b
Fang <82165052+fangenhui520@users.noreply.github.com>
2026-04-08 23:00:28 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,9 +17,3 @@
 #rm -rf ./package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/fangenhui520/luci-theme-argon.git package/lean/luci-theme-argon
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 
-sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
-./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
-rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,sing*,smartdns} feeds/packages/utils/v2dat feeds/packages/lang/golang
-git clone https://github.com/kenzok8/golang -b 1.26 feeds/packages/lang/golang
-./scripts/feeds install -a
